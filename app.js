b5065d8e4c4addfb1f1d79e928b59a2036f647ca
STASkee <stasdenysenkofrontend@gmail.com>
2023-04-19 16:37:46 +0300

    dref

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const server = http.createServer((req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  const result = 2 + 2;
-  res.end(`2 + 2 = ${result}`);
+  const result = 2 + 3;
+  res.end(`2 + 3 = ${result}`);
 });
 
 const port = 3000;
